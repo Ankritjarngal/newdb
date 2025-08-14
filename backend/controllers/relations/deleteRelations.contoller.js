517338edd8752c4974943fc443f21697b2e655f1
ankrit <jarngalankrit@gmail.com>
2025-08-14 16:49:07 +0530

    is setup done who knows

--- backend/controllers/relations/deleteRelations.contoller.js
+++ backend/controllers/relations/deleteRelations.contoller.js
@@ -1,6 +1,6 @@
 exports.deleteRelation = async (req, res) => {
     const{node1, node2, relationType} = req.body;
   return res.status(200).json({
-    message: `will delete the relation bw the ${node1} and ${node2}`,
+    message: `will delete the relation bw the ${node1} and ${node2} of type ${relationType}`,
   });
 }
